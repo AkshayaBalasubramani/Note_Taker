cdde14e5162456d7a6bebb55b9292aee9352f183
SISAAkshayaB <akshaya.b@sisainfosec.com>
2026-05-29 14:23:06 +0530

    backend url attached

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,8 +3,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiURL="/choreo-apis/note-taker/backend/v1"
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL ?? apiURL,
 });
 
 api.interceptors.request.use(
